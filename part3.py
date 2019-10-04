3041298e9c4d4b9586238c639c00523f10d01e03
Denis Urakanov <mcquicklyss@gmail.com>
2019-10-04 15:57:05 +0500

    Adding part3

--- part3.py
+++ part3.py
@@ -6,12 +6,9 @@ import matplotlib.pyplot as plt
 
 path = 'ex1data1.txt'
 data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
-data.head()
-data.describe()
 
 data.plot(kind='scatter', x='Population', y='Profit', figsize=(12,8))
 plt.show()
-
 
 data.insert(0, 'Ones', 1)
 
@@ -25,26 +22,18 @@ y = np.matrix(y.values)
 model = linear_model.LinearRegression()
 model.fit(X, y)
 
-
-g, cost = gradientDescent(X, y, theta, alpha, iters)
-print(g)
+f = model.predict(X)
 
 x = np.linspace(data.Population.min(),
                 data.Population.max(), 100)
-f = model.predict(X)
+f1 = f[0,0] + (f[1, 0] * x)
 
-fig, ax = plt.subplots(figsize=(12,8))
-ax.plot(x, f, 'r', label='Prediction')
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.plot(x, f1, 'r', label='Prediction')
 ax.scatter(data.Population, data.Profit, label='Training Data')
 ax.legend(loc=2)
 ax.set_xlabel('Population')
 ax.set_ylabel('Profit')
 ax.set_title('Predicted Profit vs. Population Size')
-
-fig, ax = plt.subplots(figsize=(12,8))
-ax.plot(np.arange(1000), cost, 'r')
-ax.set_xlabel('Iterations')
-ax.set_ylabel('Cost')
-ax.set_title('Error vs. Training Epoch')
 
 plt.show()
